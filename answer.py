bca3f590dacea9d28038d66b2671ee7cb08244af
anishadesai5 <anishadesai1811@gmail.com>
2021-05-19 18:02:26 -0400

    First commit - updated model

--- answer.py
+++ answer.py
@@ -1,12 +1,16 @@
+#importing libraries
 import pandas as pd
 from transformers.pipelines import pipeline
 
-hg_comp = pipeline('question-answering', model="distilbert-base-uncased-distilled-squad", tokenizer="distilbert-base-uncased-distilled-squad")
+#Use huggingface transformers
+hg_comp = pipeline('question-answering', model="mrm8488/bert-small-finetuned-squadv2", tokenizer="mrm8488/bert-small-finetuned-squadv2")
 
+#Input the data
 data = pd.read_csv('examples.csv')
 
+#Answer the question
 for idx, row in data.iterrows():
     context = row['context']
     question = row['question']
     answer = hg_comp({'question': question, 'context': context})['answer']
-    print(answer)
+    print("The answer to question number",(idx+1),"is",answer)
