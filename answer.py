d439dc33971c1c3eea3ba5d273535a630287d547
Ayush Maheshwari <69768815+am11917@users.noreply.github.com>
2021-05-20 01:37:24 -0400

    Added Comments and New transformation Models
    Added New Transformation Models and Printing Question, Answers and Score of the answer to compare the answers across different models based on the score generated

--- answer.py
+++ answer.py
@@ -1,7 +1,10 @@
 import pandas as pd
 from transformers.pipelines import pipeline
+
+#importing the file using pandas library
 data = pd.read_csv('Example_Data - Sheet1.csv')
 
+#using transformer model bert large uncased masking to predict the answer to our question and also getting the score of the answer
 hg_comp = pipeline('question-answering', model="bert-large-uncased-whole-word-masking-finetuned-squad", tokenizer="bert-large-uncased-whole-word-masking-finetuned-squad")
 answer = []
 score = []
@@ -14,6 +17,11 @@ for idx, row in data.iterrows():
     score.append(hg_comp({'question': question, 'context': context})['score'])
 print("")
 print ("                                      ####### MODEL - BERT LARGE UNCASED WHOLE WORD MASKING FINE TUNED #######")
+
+#printing the question, answer and score in the following format for the executed model
+# QUESTION             | ANSWER   | SCORE
+# What is this course? | MGMT 590 | 0.9565623 
+
 titles = ['QUESTION','ANSWER', 'SCORE']
 
 row_format = '{:<50}|{:<80}|{:<30}'
@@ -22,7 +30,7 @@ for item in zip(questions, answer, score):
     print (row_format.format(*item))
 
 
-    
+#using transformer model distilbert base uncased to predict the answer to our question and also getting the score of the answer  
 hg_comp_1 = pipeline('question-answering', model="distilbert-base-uncased-distilled-squad", tokenizer="distilbert-base-uncased-distilled-squad")
 answer_1 = []
 score_1 = []
@@ -36,11 +44,16 @@ for idx, row in data.iterrows():
 print("")
 print ("                                      ####### MODEL - DISTILBERT BASE UNCASED DISTILLED #######")
 
+#printing the question, answer and score in the following format for the executed model
+# QUESTION             | ANSWER   | SCORE
+# What is this course? | MGMT 590 | 0.9565623 
+
 row_format = '{:<50}|{:<80}|{:<30}'
 print (row_format.format(*titles))
 for item in zip(questions_1, answer_1, score_1):
     print (row_format.format(*item))
-    
+
+#using transformer model bert based modified by csarron to predict the answer to our question and also getting the score of the answer  
 hg_comp_2 = pipeline('question-answering', model="csarron/bert-base-uncased-squad-v1", tokenizer="csarron/bert-base-uncased-squad-v1")
 answer_2 = []
 score_2 = []
@@ -54,6 +67,9 @@ for idx, row in data.iterrows():
 
 print("") 
 print ("                                      ####### MODEL - CSARRON's BERT BASE UNCASED #######")
+#printing the question, answer and score in the following format for the executed model
+# QUESTION             | ANSWER   | SCORE
+# What is this course? | MGMT 590 | 0.9565623 
 row_format = '{:<50}|{:<80}|{:<30}'
 print (row_format.format(*titles))
 for item in zip(questions_2, answer_2, score_2):
