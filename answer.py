0d98103fd2ea2bd3b77c6458310484963cc88636
IshaAgar <84544148+IshaAgar@users.noreply.github.com>
2021-05-20 17:23:45 -0400

    Update answer.py
    Updated the Model and Tokenizer to pretrained "bert-large-uncased-whole-word-masking-finetuned-squad" and added the comments to the script.

--- answer.py
+++ answer.py
@@ -1,12 +1,20 @@
+#importing library
 import pandas as pd
+from transformers import AutoTokenizer, AutoModelForQuestionAnswering
 from transformers.pipelines import pipeline
+import torch
 
-hg_comp = pipeline('question-answering', model="distilbert-base-uncased-distilled-squad", tokenizer="distilbert-base-uncased-distilled-squad")
+#Modelling
+Tokenizer = AutoTokenizer.from_pretrained("bert-large-uncased-whole-word-masking-finetuned-squad")
+Model = AutoModelForQuestionAnswering.from_pretrained("bert-large-uncased-whole-word-masking-finetuned-squad")
+
+comp = pipeline('question-answering', model= Model, tokenizer = Tokenizer)
 
 data = pd.read_csv('examples.csv')
 
 for idx, row in data.iterrows():
     context = row['context']
     question = row['question']
-    answer = hg_comp({'question': question, 'context': context})['answer']
+    answer = comp({'question': question, 'context': context})['answer']
+    print(question)
     print(answer)
