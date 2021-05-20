70bbd482e700b0bff286b084cebab37f85a2006d
ajaashirjain <67500400+ajaashirjain@users.noreply.github.com>
2021-05-20 16:23:34 -0400

    ImprovedModel.py
    Hi,
    I tried 5 different models and chose the "deepset/roberta-base-squad2" model as it gave the most accurate answer when the answers were descriptive. The results were the same and accurate when answers were of one word, but the main difference was eminent in subjective answers. Output prints the question along with the answers.

--- answer.py
+++ answer.py
@@ -1,13 +1,15 @@
 import pandas as pd
 from transformers.pipelines import pipeline
     
-hg_comp = pipeline('question-answering', model="bert-large-uncased-whole-word-masking-finetuned-squad", tokenizer="bert-large-uncased-whole-word-masking-finetuned-squad")
+hg_comp = pipeline('question-answering', model="deepset/roberta-base-squad2", tokenizer="deepset/roberta-base-squad2")
 
 data = pd.read_csv('Example_Data1.csv')
-    
+i= 0 
 for idx, row in data.iterrows():
+    i = i+1
     context = row['context']
     question = row['question']
     answer = hg_comp({'question': question, 'context': context})['answer']
-    print('Q.',question)
+
+    print('Q.',i,question)
     print('Ans.',answer)
