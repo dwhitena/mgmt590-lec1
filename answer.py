f0fca8fe924ab9ab1c6554130f2cbd31b628ec52
abhinavreddy2008 <84475578+abhinavreddy2008@users.noreply.github.com>
2021-05-19 20:27:09 -0400

    Update answer.py

--- answer.py
+++ answer.py
@@ -1,12 +1,29 @@
+#Importing Libraries
 import pandas as pd
+import numpy as np
+
+# Importing Pipeline from transformer library
 from transformers.pipelines import pipeline
 
-hg_comp = pipeline('question-answering', model="distilbert-base-uncased-distilled-squad", tokenizer="distilbert-base-uncased-distilled-squad")
+# Calling the pipeline function by giving model argument as "bert-base-multilingual-cased" and tokenizer argument as "distilbert-base-uncased-distilled-squad"
+hg_BERT = pipeline('question-answering', model="bert-base-multilingual-cased", tokenizer="distilbert-base-uncased-distilled-squad")
 
+# Loading the dataset
 data = pd.read_csv('examples.csv')
 
-for idx, row in data.iterrows():
+#Creating a empty list to store the answers
+answer_li = []
+##Lopping over the dataframe df and feeding the question and context as an input into the pre trained model to generate respective answers 
+##and finally storing those answers into a list
+for i, row in df.iterrows():
     context = row['context']
     question = row['question']
-    answer = hg_comp({'question': question, 'context': context})['answer']
-    print(answer)
+    answer = hg_BERT({'question': question, 'context': context})['answer']
+    answer_li.append(answer)
+
+# Creating a new dataframe to store the data from Context, Question and Answers
+df_new = pd.DataFrame()
+df_new['context'] = df['context']
+df_new['question'] = df['question']
+df_new['answer'] = answer_li
+df_new
