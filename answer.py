ee81d2a71bd08bdbe6edbef4c41810852a846491
soumik96 <83850819+soumik96@users.noreply.github.com>
2021-05-20 16:57:16 -0400

    Easy-to-use UI for Non technical clients
    Hey!!
    Instead of inputting multiple questions at once in an excel sheet and finding the answers by running the python code, I created a web interface in which clients from a non technical background can input their question and context in the "Question" and "Paragraph" section of the webpage respectively. The answer would be displayed in the "Output" section.
    After running the code here, you will be redirected to a new tab on your browser, post which, you can present it to the client to experiment with the question-answering model.

--- answer.py
+++ answer.py
@@ -1,12 +1,16 @@
 import pandas as pd
 from transformers.pipelines import pipeline
+pip install gradio
+import gradio as gr
 
-hg_comp = pipeline('question-answering', model="distilbert-base-uncased-distilled-squad", tokenizer="distilbert-base-uncased-distilled-squad") #Using the Distilbert-base-uncased-distilled-squad model
 
-data = pd.read_csv('examples.csv')
+def answer_question(question: str, paragraph: str):
+    input = {
+        "question": question,
+        "context": paragraph
+    }
+    hg_comp = pipeline('question-answering', model="distilbert-base-uncased-distilled-squad", tokenizer="distilbert-base-uncased-distilled-squad") #Using the Distilbert-base-uncased-distilled-squad model
+    return hg_comp(input)['answer']
 
-for idx, row in data.iterrows():
-    context = row['context']
-    question = row['question']
-    answer = hg_comp({'question': question, 'context': context})['answer']
-    print(answer)
+
+gr.Interface(fn=answer_question, inputs=["textbox", "text"], outputs= "text").launch(share = True, inbrowser = True)
