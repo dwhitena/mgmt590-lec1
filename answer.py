9e109245a350ddaa048bc07b014ed1023bee24b8
sahi0911 <83843265+sahi0911@users.noreply.github.com>
2021-05-20 15:40:01 -0400

    Update answer.py

--- answer.py
+++ answer.py
@@ -1,12 +1,16 @@
+#Importing Libraries
 import pandas as pd
 from transformers.pipelines import pipeline
 
-hg_comp = pipeline('question-answering', model="distilbert-base-uncased-distilled-squad", tokenizer="distilbert-base-uncased-distilled-squad")
+#Implementing Bert-Large-Uncased Model
+hg_comp = pipeline('question-answering', model="bert-large-uncased", tokenizer="bert-large-uncased")
 
+#Reading Data
 data = pd.read_csv('examples.csv')
 
+#Display Answer
 for idx, row in data.iterrows():
     context = row['context']
     question = row['question']
     answer = hg_comp({'question': question, 'context': context})['answer']
-    print(answer)
+    print("The answer to the question: \n" + question + "Ans: " + answer)
