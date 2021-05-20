b27388a69c8a2fd37157d73d34c975dbeb7e3636
Harshal Khona <54956135+HarshalKhona95@users.noreply.github.com>
2021-05-20 16:15:20 -0400

    Updated Model with Comments
    Updated the model - https://huggingface.co/deepset/xlm-roberta-large-squad2
    Added comments for better readability

--- answer.py
+++ answer.py
@@ -1,14 +1,14 @@
+#Importing Libraries
 import pandas as pd
 from transformers.pipelines import pipeline
-
-hg_comp = pipeline('question-answering', model="distilbert-base-uncased-distilled-squad", tokenizer="distilbert-base-uncased-distilled-squad")
-
+#Modeling (Changed the model to a different one from Hugging Face - Got the same answers for all 5 questions; Link - https://huggingface.co/deepset/xlm-roberta-large-squad2)
+hg_comp = pipeline('question-answering', model="deepset/xlm-roberta-large-squad2", tokenizer="deepset/xlm-roberta-large-squad2")
+#Reading File
 data = pd.read_csv('examples.csv')
-
+#Iterating Over Data
 for idx, row in data.iterrows():
     context = row['context']
     question = row['question']
     answer = hg_comp({'question': question, 'context': context})['answer']
+    print(question)
     print(answer)
-    
-#Thank You
