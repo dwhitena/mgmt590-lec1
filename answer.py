c54ec3585d194996fa016e715402efb71fb30ef6
kaashyap10 <84420328+kaashyap10@users.noreply.github.com>
2021-05-20 16:37:49 -0400

    Assignment 1 - 0032112635
    Changed the model to "bert-large-cased-whole-word-masking-finetuned-squad" and made changes to the print statement.

--- answer.py
+++ answer.py
@@ -10,3 +10,10 @@ for idx, row in data.iterrows():
     question = row['question']
     answer = hg_comp({'question': question, 'context': context})['answer']
     print('The answer to the question "' + question + '" is "' + answer + '"')
+
+#Answers:
+#The answer to the question "who won the match sri lanka or india?" is "India"
+#The answer to the question "what was the prison called in the green mile?" is "Cold Mountain Penitentiary"
+#The answer to the question "what is the widest highway in north america?" is "Highway 401"
+#The answer to the question "what makes the center of an atom stable?" is "The closer an electron is to the nucleus, the greater the attractive force"
+#The answer to the question "who did holly matthews play in waterloo rd?" is "Leigh-Ann Galloway"
