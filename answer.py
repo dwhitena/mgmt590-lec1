d7f5ec30b26813cac4e8e1df2d3cae1d50510f9e
anishadesai5 <anishadesai1811@gmail.com>
2021-05-20 16:48:10 -0400

    Second commit - Assignment 1

--- answer.py
+++ answer.py
@@ -14,3 +14,13 @@ for idx, row in data.iterrows():
     question = row['question']
     answer = hg_comp({'question': question, 'context': context})['answer']
     print("The answer to question number",(idx+1),"is",answer)
+
+#Use huggingface transformers
+hg_comp = pipeline('question-answering', model="distilbert-base-uncased-distilled-squad", tokenizer="distilbert-base-uncased-distilled-squad")
+
+#Answer the question
+for idx, row in data.iterrows():
+    context = row['context']
+    question = row['question']
+    answer = hg_comp({'question': question, 'context': context})['answer']
+    print(answer)
