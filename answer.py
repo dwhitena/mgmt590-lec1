576ba04f230b21a30c1d26f806532a449a882247
ajaashirjain <67500400+ajaashirjain@users.noreply.github.com>
2021-05-20 16:40:59 -0400

    UpdateAnswer.py
    Hi,
    Tried 5 different models and used "deepset/roberta-base-squad2" as it gave good accuracy in all cases. Also, changed the output to show questions along with question number and answers.

--- answer.py
+++ answer.py
@@ -1,12 +1,15 @@
 import pandas as pd
 from transformers.pipelines import pipeline
+    
+hg_comp = pipeline('question-answering', model="deepset/roberta-base-squad2", tokenizer="deepset/roberta-base-squad2")
 
-hg_comp = pipeline('question-answering', model="distilbert-base-uncased-distilled-squad", tokenizer="distilbert-base-uncased-distilled-squad")
-
-data = pd.read_csv('examples.csv')
-
+data = pd.read_csv('Example_Data1.csv')
+i= 0 
 for idx, row in data.iterrows():
+    i = i+1
     context = row['context']
     question = row['question']
     answer = hg_comp({'question': question, 'context': context})['answer']
-    print(answer)
+
+    print('Q.',i,question)
+    print('Ans.',answer)
