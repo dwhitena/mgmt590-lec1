4146c0112d3c1bbd494e5966a0d43dab5fccfeca
rrajwade007 <rrajwade@purdue.edu>
2021-05-20 16:33:33 -0400

    Update answer.py
    I have applied a different model and printed out scores for both models.
    I observed that in all cases, the model used in class did better than the new model used. I also used the terminal to link the GitHub account and add, commit and push the new file.

--- answer.py
+++ answer.py
@@ -3,12 +3,25 @@ from transformers.pipelines import pipeline
 
 hg_comp = pipeline('question-answering', model="distilbert-base-uncased-distilled-squad", tokenizer="distilbert-base-uncased-distilled-squad")
 
-data = pd.read_csv('examples.csv')
+data = pd.read_csv('/Users/vb/Documents/College/Production Scale Data Products/examples.csv')
+
+hg_comp2 = pipeline(model="bert-large-cased-whole-word-masking-finetuned-squad", tokenizer="bert-large-cased-whole-word-masking-finetuned-squad", task="question-answering")
+
+for idx, row in data.iterrows():
+    context = row['context']
+    question = row['question']
+    answer = hg_comp2({'question': question, 'context': context})['answer']
+    score = hg_comp2({'question': question, 'context': context})['score']
+    print(question, end='   :   ')
+    print(answer)
+    print(score)
 
 for idx, row in data.iterrows():
     context = row['context']
     question = row['question']
     answer = hg_comp({'question': question, 'context': context})['answer']
+    score = hg_comp({'question': question, 'context': context})['score']
+    print(question, end='   :   ')
     print(answer)
+    print(score)
 #This was very fun!
-#First Change using the GitHub GUI
