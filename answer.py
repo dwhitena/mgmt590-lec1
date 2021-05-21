f0f3c5fbaa2d2608223478ad02792751f0dcbce6
Sai Teja Pasula <saitejapasula@gmail.com>
2021-05-20 23:44:22 -0400

    Add files via upload

--- answer.py
+++ answer.py
@@ -1,12 +1,21 @@
 import pandas as pd
 from transformers.pipelines import pipeline
 
-hg_comp = pipeline('question-answering', model="distilbert-base-uncased-distilled-squad", tokenizer="distilbert-base-uncased-distilled-squad")
+model = "madlag/bert-base-uncased-squadv1-x2.32-f86.6-d15-hybrid-v1"
+tokenizer = "madlag/bert-base-uncased-squadv1-x2.32-f86.6-d15-hybrid-v1"
 
-data = pd.read_csv('examples.csv')
+hg_comp = pipeline('question-answering',
+                   model=model,
+                   tokenizer=tokenizer
+                   )
+
+path_str = "C:/Users/saite/Desktop/Masters/1.Purdue/Course Material/3.Sum Mod 1/MGMT 590 PSD/2.Assignments/examples.csv"
+
+data = pd.read_csv(path_str)
 
 for idx, row in data.iterrows():
     context = row['context']
     question = row['question']
     answer = hg_comp({'question': question, 'context': context})['answer']
-    print(answer)
+    print('Question',idx+1,": ",question)
+    print("Answer",idx+1,": ",answer)
