5bba71bc45d9d7ab1de7fac92d9d509e9fb1964b
Nipurn-Minocha <nipurn.minocha92@gmail.com>
2021-05-20 22:21:51 -0400

    Changed model and printed question answer in a sequence manner

--- answer.py
+++ answer.py
@@ -1,12 +1,13 @@
 import pandas as pd
 from transformers.pipelines import pipeline
 
-hg_comp = pipeline('question-answering', model="distilbert-base-uncased-distilled-squad", tokenizer="distilbert-base-uncased-distilled-squad")
+hg_comp = pipeline('question-answering', model="bert-large-uncased-whole-word-masking-finetuned-squad", tokenizer="bert-large-uncased-whole-word-masking-finetuned-squad")
 
-data = pd.read_csv('examples.csv')
+data = pd.read_csv('Example_Data.csv')
 
 for idx, row in data.iterrows():
     context = row['context']
     question = row['question']
     answer = hg_comp({'question': question, 'context': context})['answer']
-    print(answer)
+    print(idx+1,question)
+    print(idx+1,answer)
