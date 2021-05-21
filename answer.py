ad592b0e085c50c895f70ed2c08112140d54b95a
unknown <kshah.shah951@gmail.com>
2021-05-20 21:07:48 -0400

    Added an improved model (has a higher exact match rate) along with the original model. This improved model will show the user the answer with a higher likelihood of being correct but will also show the answer based on the original model just for comparison. The print statement was updated to reprint the question and show the answers based on the two models. In addition to these changes, short comments were added to show what each code segment is doing.

--- answer.py
+++ answer.py
@@ -1,11 +1,23 @@
-import pandas as pd from transformers.pipelines import pipeline
+#importing
+import pandas as pd   
+from transformers.pipelines import pipeline
 
-hg_comp = pipeline('question-answering', model="bert-large-uncased-whole-word-masking-finetuned-squad", tokenizer="bert-large-uncased-whole-word-masking-finetuned-squad")
+#original model
+hg_comp = pipeline('question-answering', model="distilbert-base-uncased-distilled-squad", tokenizer="distilbert-base-uncased-distilled-squad")
+#improved model
+hg_comp_new = pipeline('question-answering', model="bert-large-uncased-whole-word-masking-finetuned-squad", tokenizer="bert-large-uncased-whole-word-masking-finetuned-squad")
 
+#reading the file
 data = pd.read_csv('examples.csv')
 
+#finding the answer to the question based on both the models
 for idx, row in data.iterrows():
     context = row['context']
     question = row['question']
     answer = hg_comp({'question': question, 'context': context})['answer']
-    print('Hello, the answer to your question is ' + answer)
+    answer_new = hg_comp_new({'question': question, 'context': context})['answer']
+    print("Question: " + question)
+    print('Answer (based on original model): ' + answer)
+    print('Answer (based on improved model): ' + answer_new)
+
+
