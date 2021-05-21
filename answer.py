9e305ab0a28612d12bc5da963bc3a6fbba8f52c7
seesaw21 <84552751+seesaw21@users.noreply.github.com>
2021-05-20 22:34:55 -0400

    Update answer.py

--- answer.py
+++ answer.py
@@ -1,12 +1,21 @@
 import pandas as pd
 from transformers.pipelines import pipeline
+import streamlit as st
 
-hg_comp = pipeline('question-answering', model="distilbert-base-uncased-distilled-squad", tokenizer="distilbert-base-uncased-distilled-squad")
+st.cache(show_spinner=False)
+def load_model():
+    hg_comp = pipeline('question-answering', model="bert-large-cased-whole-word-masking-finetuned-squad", tokenizer="bert-large-cased-whole-word-masking-finetuned-squad")
+    return hg_comp
 
 data = pd.read_csv('examples.csv')
 
-for idx, row in data.iterrows():
-    context = row['context']
-    question = row['question']
-    answer = hg_comp({'question': question, 'context': context})['answer']
-    print(answer)
+nlp_pipe = load_model()
+st.header("QA App")
+
+question = st.text_input(label='Question')
+text = st.text_area(label="Reference")
+
+
+if (not len(text)==0) and (not len(question)==0):
+    x_dict = nlp_pipe(context=text,question=question)
+    st.text('Answer :'+ x_dict['answer'])
