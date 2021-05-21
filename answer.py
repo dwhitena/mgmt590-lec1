b937f6727226ebb6c17fbfbadd34621bc6784b1b
EshaKaushal <212esha@gmail.com>
2021-05-20 23:03:18 -0400

    Update answer.py

--- answer.py
+++ answer.py
@@ -2,7 +2,7 @@ import pandas as pd
 from transformers.pipelines import pipeline
 
 data = pd.read_csv('examples.csv')
-model_list = ["deepset/roberta-base-squad2", "bert-large-uncased-whole-word-masking-finetuned-squad", "mrm8488/bert-tiny-finetuned-squadv2"]
+model_list = ["distilbert-base-uncased-distilled-squad","deepset/roberta-base-squad2","bert-large-uncased-whole-word-masking-finetuned-squad","mrm8488/bert-tiny-finetuned-squadv2"]
 model_num = len(model_list)
 for idx, row in data.iterrows():
     context = row['context']
@@ -10,6 +10,7 @@ for idx, row in data.iterrows():
     print("The question is", question)
     model_score = {}
     model_answer = {}
+    # generate answer using different models
     for i in range(model_num):
         print("Predicting answers using model -", model_list[i])
         hg_comp = pipeline(model=model_list[i], tokenizer=model_list[i], task="question-answering")
@@ -19,6 +20,8 @@ for idx, row in data.iterrows():
         #print("The score is",score)
         model_score[model_list[i]] = score
         model_answer[model_list[i]] = answer
+
+    #Select answer with highest confidence score
     best_model = max(model_score, key=model_score.get)
     print("Best answer for this question was given by model - ",best_model)
     print("Answer with highest confidence score - ",model_answer[best_model])
