379956b3cdeda2dd4f217be573b2ab5086ffc54f
Shashwat-1995 <67730480+Shashwat-1995@users.noreply.github.com>
2021-05-20 22:59:27 -0400

    Update answer.py

--- answer.py
+++ answer.py
@@ -2,7 +2,6 @@ import pandas as pd
 from transformers.pipelines import pipeline
     
 hg_comp = pipeline('question-answering', model="bert-large-uncased-whole-word-masking-finetuned-squad", tokenizer="bert-large-uncased-whole-word-masking-finetuned-squad")
-
 data = pd.read_csv('Example_Data1.csv')
     
 for idx, row in data.iterrows():
