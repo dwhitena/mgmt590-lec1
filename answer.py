0462a222e0ac2368f5c62898f187a5c8c890b311
sabitrarudra <84482477+sabitrarudra@users.noreply.github.com>
2021-05-20 23:32:25 -0400

    MGMT59000-003 Assignment 1
    1 Changed the model to a more descriptive one after going through some other question answering models 
    2 A glob code was added to read multiple files at once 
    3 Included an interactive web interface using python library wherein a user can just find an answer to a single question in an interactive and easy-to-use way. The user will just need to mention the question and the context for the same in the text box provided in the interface and hit the submit button to get the answer for the same.
    4 Changed the format of displaying the Answer
    5 Added Comments to the Code 
    5 Added ReadMe file explaining the model

--- answer.py
+++ answer.py
@@ -1,12 +1,33 @@
+# Importing Necessary Libraries
 import pandas as pd
+import glob
+import gradio as gr
 from transformers.pipelines import pipeline
 
-hg_comp = pipeline('question-answering', model="distilbert-base-uncased-distilled-squad", tokenizer="distilbert-base-uncased-distilled-squad")
+# Replaced old model with a new more efficient model 
+model = pipeline('question-answering', model="bert-large-uncased-whole-word-masking-finetuned-squad", tokenizer="bert-large-uncased-whole-word-masking-finetuned-squad")
 
-data = pd.read_csv('examples.csv')
+#Reading output file using for loop and glob function 
+data = []
+for filename in glob.glob('*.csv'):
+    im = pd.read_csv('Example_Data - Sheet1.csv')
+    data.append(im)
+for data1 in data:
+    for idx, row in data1.iterrows():
+        context = row['context']
+        question = row['question']
+        answer = model({'question': question, 'context': context})['answer']
+        print('Q.',question)
+        print('Ans.',answer)
 
-for idx, row in data.iterrows():
-    context = row['context']
-    question = row['question']
-    answer = hg_comp({'question': question, 'context': context})['answer']
-    print(answer)
+## Building web user Interface
+
+def getanswer(que: str, context: str):
+    input = {
+        "question": que, "context": context
+    }
+    return model(input)['answer']
+
+gr.Interface(fn=getanswer, inputs=["textbox", "text"], outputs= "text" ).launch()
+        
+
